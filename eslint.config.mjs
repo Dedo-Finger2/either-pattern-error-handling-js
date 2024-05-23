ba302cc75ec5997fe3c5f48cfb759677d298f9af
Greg <antonioimportant@gmail.com>
2024-05-23 15:31:52 -0300

    chore: init

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  {
+    languageOptions: { 
+      globals: globals.browser 
+    },
+    rules: {
+      semi: "error",
+      "no-constructor-return": "error",
+      "no-dupe-class-members": "error",
+      "no-dupe-else-if": "error",
+      "no-duplicate-imports": "error",
+      "no-setter-return": "error",
+      "no-unused-private-class-members": "error",
+      camelcase: "error",
+      "default-case": "error",
+      eqeqeq: "error",
+      "no-var": "error",
+    },
+  },
+  pluginJs.configs.recommended,
+];
