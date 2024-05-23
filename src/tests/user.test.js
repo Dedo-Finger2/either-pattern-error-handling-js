6339bf1cd6b7e07cf5355f5b805b6b9fa66e1c29
Greg <antonioimportant@gmail.com>
2024-05-23 16:01:58 -0300

    feat: ensure user creation is done with valid data

---
+++ src/tests/user.test.js
@@ -0,0 +1,22 @@
+import { describe, it, expect } from "vitest";
+import { User } from "./../core/domain/entities/user.js";
+
+/** @typedef {({ name: string, email: string, password: string })} UserDto  */
+
+describe("User Entity Class", () => {
+  it("should create a new user when valid data is passed", () => {
+    /** @type {UserDto} */
+    const userDto = {
+      name: "fake_name",
+      email: "valid_fake@email.com",
+      password: "fake_valid_password",
+    };
+
+    const sut = new User(userDto);
+
+    expect(sut).toBeInstanceOf(User);
+    expect(sut.name).toBe(userDto.name);
+    expect(sut.email).toBe(userDto.email);
+    expect(sut.password).toBe(userDto.password);
+  });
+});
